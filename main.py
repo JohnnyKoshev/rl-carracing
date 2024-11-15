bbefa4e93a783977c9a8694b98cedb7288ab3404
JohnnyKoshev <komilzonu5@gmail.com>
2024-11-16 01:45:41 +0500

    feat: modified multicarrracing and working dqn training script

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import numpy as np
 import gym
-import gym_multi_car_racing
 from pyglet.window import key
 import random
 
+from stable_baselines3 import DQN
+
+gym.register(
+    id='MultiCarRacing-v0',
+    entry_point='merger:MultiCarRacing',
+    max_episode_steps=1000,
+    reward_threshold=900
+)
+
 
 def random_policy(observation):
-    # action[0] is steering: range [-1, 1]
-    # action[1] is gas: range [0, 1]
-    # action[2] is brake: range [0, 1]
     return np.array([random.uniform(-1, 1), random.uniform(0, 1), random.uniform(0, 1)])
+
+
+def discrete_random_policy(observation):
+    return random.randint(0, 4)
+
+
+def model_policy(observation, model):
+    obs = np.copy(observation)
+    pred = model.predict(obs)
+
+    if isinstance(pred[0], np.ndarray):  # continuous action
+        return pred[0]  # Return the continuous action
+    else:  # discrete action
+        return pred[0]  # Return the discrete action (an integer)
 
 
 if __name__ == "__main__":
@@ -17,7 +36,15 @@ if __name__ == "__main__":
 
     CAR_CONTROL_KEYS = [[key.LEFT, key.RIGHT, key.UP, key.DOWN]]
 
-    actions = np.zeros((NUM_CARS, 3))
+    # actions = np.zeros((NUM_CARS, 3))
+    actions = [
+        [0.0, 0.0, 0.0],
+        0
+    ]
+
+    model = DQN.load("DQN_RL_100")
+
+    print("Model loaded")
 
 
     def key_press(k, mod):
@@ -39,33 +66,25 @@ if __name__ == "__main__":
         if k == CAR_CONTROL_KEYS[0][3]: actions[0][2] = 0
 
 
-
     env = gym.make("MultiCarRacing-v0", num_agents=NUM_CARS, direction='CCW',
-                   use_random_direction=True, backwards_flag=True,
-                   h_ratio=0.25, use_ego_color=False)
+                   use_random_direction=True,
+                   use_ego_color=True, continuous_actions=[True, False], car_labels=["Johnny", "DQN"])
 
     obs = env.reset()
-
 
     for viewer in env.viewer:
         viewer.window.on_key_press = key_press
         viewer.window.on_key_release = key_release
 
-    record_video = False
-    if record_video:
-        from gym.wrappers.monitor import Monitor
-
-        env = Monitor(env, '/tmp/video-test', force=True)
-
-    isopen = True
+    is_open = True
     stopped = False
-    while isopen and not stopped:
+    while is_open and not stopped:
         obs = env.reset()
         total_reward = np.zeros(NUM_CARS)
         steps = 0
         restart = False
         while True:
-            actions[1] = random_policy(obs[1])
+            actions[1] = model_policy(obs[1], model)
 
             obs, r, done, info = env.step(actions)
             total_reward += r
@@ -76,7 +95,7 @@ if __name__ == "__main__":
                 # plt.imshow(s)
                 # plt.savefig("test.jpeg")
             steps += 1
-            isopen = env.render().all()
-            if stopped or done or restart or isopen == False:
+            is_open = env.render().all()
+            if stopped or done or restart or is_open == False:
                 break
     env.close()
